58c1aa2bb0968e5c006a5f42d889be7498ae8c63
Vinicius Schmitz <viniciusschmitzwork@gmail.com>
2024-08-19 18:14:17 -0300

    inicio

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// 3 Processadores
+
+// Memória RAM [50]
+
+// Memória Cache 1 [5]
+// Memória Cache 2 [5]
+// Memória Cache 3 [5]
+
+
+void main()
+{
+    printf("\nBEM VINDO AO SIMULADOR DE MEMÓRIA CACHE GERENCIADA POR PROTOCOLO MESI");
+
+
+
+}
